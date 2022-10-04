c7ae982963b7a1d53594cc748b3e15c7baaa047d
bryant tunbutr <bryantt23@users.noreply.github.com>
2022-10-04 00:47:40 -0700

    create server

--- server.js
+++ server.js
@@ -7,6 +7,15 @@ let server;
 
 // Your code here
 
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 5000;
+server = http.createServer((req, res) => {});
+
+server.listen(port, hostname, () => {
+  console.log(`Successfully started the server on port 5000`);
+});
+
 /******************************************************************************/
 /******************* DO NOT CHANGE THE CODE BELOW THIS LINE *******************/
 
