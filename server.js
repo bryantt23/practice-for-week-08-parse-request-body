f19e7e64144b9ebcb86cc976605505c3964c6376
bryant tunbutr <bryantt23@users.noreply.github.com>
2022-10-05 01:01:53 -0700

    fin
    Review the steps to parse the request body in the Parsing the Body of the Request reading.

--- server.js
+++ server.js
@@ -6,15 +6,23 @@ let server;
 /******************* DO NOT CHANGE THE CODE ABOVE THIS LINE *******************/
 
 // Your code here
-
 const http = require('http');
 const hostname = '127.0.0.1';
 const port = 5000;
 server = http.createServer((req, res) => {
-  console.log(`${req.method} ${req.url}`);
+  let reqBody = '';
+  req.on('data', data => {
+    reqBody += data;
+  });
+
+  req.on('end', () => {
+    console.log(reqBody);
+  });
   // your code for assembling the request body string...
   req.on('end', () => {
     // your code for parsing the request body string into an object...
+    const object = parseBody(reqBody);
+    req.body = object;
 
     sendFormPage(req, res); // needs to be called even if the request doesn't have a body
   });
